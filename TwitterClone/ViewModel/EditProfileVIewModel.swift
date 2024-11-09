115f2f9ad42712d7bf1fbbfef54e0cd89298a01f
emmanuelanene <emmanuel.anene07@gmail.com>
2024-11-08 17:30:30 -0800

    EditProfileViewModel

--- TwitterClone/ViewModel/EditProfileVIewModel.swift
+++ TwitterClone/ViewModel/EditProfileVIewModel.swift
@@ -5,4 +5,56 @@
 //  Created by Emmanuel Anene on 08/11/2024.
 //
 
-import Foundation
+import UIKit
+
+enum EditProfileOptions: Int, CaseIterable {
+    case fullname
+    case username
+    case bio
+    
+    var description: String {
+        switch self {
+            case .username: return "Username"
+            case .fullname: return "Name"
+            case .bio: return "Bio"
+        }
+    }
+}
+
+
+struct EditProfileViewModel {
+    private let user: User
+    let option: EditProfileOptions
+    
+    var titleText: String {
+        return option.description
+    }
+    
+    var optionValue: String? {
+        switch option {
+        case .fullname:
+            return user.fullName
+        case .username:
+            return user.useername
+        case .bio:
+            return user.bio
+        }
+    }
+    
+    var shouldHideTextField: Bool {
+        return option == .bio
+    }
+    
+    var shouldHideTextView: Bool {
+        return option != .bio
+    }
+    
+    var shouldHidePlaceholderLabel: Bool {
+        return user.bio != nil
+    }
+    
+    init(user: User, option: EditProfileOptions) {
+        self.user = user
+        self.option = option
+    }
+}
--- TwitterClone/ViewModel/TweetViewModel.swift
+++ TwitterClone/ViewModel/TweetViewModel.swift
@@ -5,4 +5,5 @@
 //  Created by Emmanuel Anene on 08/11/2024.
 //
 
-import Foundation
+import UIKit
+
